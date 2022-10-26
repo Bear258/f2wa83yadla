cf044101a1bccd5d66393e0f081f788822d4dc3a
Bear258 <s556383@nwmissouri.edu>
2022-10-26 12:28:02 -0500

    updated

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var times=0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 times=times+1;
   res.send('users accesses are:'+times);
-  var times=times+1;
+  
 });
 
 module.exports = router;
