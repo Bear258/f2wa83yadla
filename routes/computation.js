5e9f525b9c9bf8e68bca78d2c33e7b1dbdf46f1e
Bear258 <s556383@nwmissouri.edu>
2022-10-26 21:44:04 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
     var latha2=Math.atanh(swarna1,swarna2);
     var latha3=Math.cbrt(swarna1);
 
-  res.render('computation', { title: 'Nikitha Vedant Madabhushi Bonus',
+  res.render('computation', { title: 'Yadla Swarna latha Bonus',
 title1:'Random Absolute Input value',swarna1:swarna1,
 title2:'Random Absolute Input value',swarna2:swarna2 ,
 latha1:latha1, latha2:latha2 ,latha3:latha3 });
